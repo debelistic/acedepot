c4574fb541f370379e25a53bc103fbf7aa872012
Victor Awotidebe <victorawotidebe@gmail.com>
2019-06-10 19:16:50 +0100

    modify auth:register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,109 +1,95 @@
 @extends('layout.app')
 
+@section('styles')
+	<link rel="stylesheet" type="text/css" href="{{asset('css/auth.css')}}" />
+@endsection
 @section('content')
-	<section class="overlape">
-	<div class="block no-padding">
-		<div data-velocity="-.1" style="background: url(images/slider2.jpg) repeat scroll 50% 422.28px transparent;" class="parallax scrolly-invisible no-parallax"></div><!-- PARALLAX BACKGROUND IMAGE -->
-			<div class="container fluid">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="inner-header">
-							<h3>In prevailing adverse economy, we position you for growth in life, career and business</h3>
-						</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	</section>
+	
     
-<section>
-	<div class="register_signin_wrapper">
-		<div class="register_signin">
-			<div class="card register_form" style="display: block; height: inherit">
-				<div class="card-header">{{ __('Register') }}</div>
+<section class="auth_form_area">
+	<main class="auth_main">
+		<div class="main_overlay">
+			<h1>In prevailing adverse economy, we position you for growth in life, career and business</h1>
+		</div>
+	</main>
+	<aside class="auth_form">
+		<h2 class="auth_header">{{ __('Register') }}</h2>
 
-				<div class="card-body">
-					<select name="role" form="user_register">
-						<option>Select Your Role</option>
-						<option value="candiate">Candiate</option>
-						<option value="contractor">Contractor</option>
-						<option value="employer">Employer</option>
-					</select>
-					@error('role')
+		<div class="">
+			<select name="role" form="user_register" class="soflow-color">
+				<option>Select Your Role</option>
+				<option value="candiate">Candiate</option>
+				<option value="contractor">Contractor</option>
+				<option value="employer">Employer</option>
+			</select>
+			@error('role')
+				<span class="invalid-feedback" role="alert">
+					<strong>{{ $message }}</strong>
+				</span>
+			@enderror
+			<form method="POST" action="{{ route('register') }}" id="user_register">
+				@csrf
+
+				<div class="auth_input">
+					<label for="first_name" class="">
+						<span class="content_name">{{ __('First Name') }}</span>
+					</label>
+					<input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+					@error('first_name')
 						<span class="invalid-feedback" role="alert">
 							<strong>{{ $message }}</strong>
 						</span>
 					@enderror
-					<form method="POST" action="{{ route('register') }}" id="user_register">
-						@csrf
-
-						<div class="form-group row form_input">
-							<input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-							<label for="first_name" class="col-md-4 col-form-label text-md-right label_name">
-								<span class="content_name">{{ __('First Name') }}</span>
-							</label>
-							@error('first_name')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-							@enderror
-						</div>
-
-						<div class="form-group row form_input">
-							<input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-							<label for="last_name" class="col-md-4 col-form-label text-md-right label_name">
-								<span class="content_name">{{ __('Last Name') }}</span>
-							</label>
-							@error('last_name')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-							@enderror
-						</div>
-
-						<div class="form-group row form_input">
-							<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-							<label for="email" class="col-md-4 col-form-label text-md-right label_name">
-								<span class="content_name">{{ __('E-Mail Address') }}</span>
-							</label>
-							@error('email')
-							<span class="invalid-feedback" role="alert">
-								<strong>{{ $message }}</strong>
-							</span>
-							@enderror
-						</div>
-
-						<div class="form-group row form_input">
-							<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-							<label for="password" class="col-md-4 col-form-label text-md-right label_name">
-								<span class="content_name">{{ __('Password') }}</span>
-							</label>
-							@error('password')
-								<span class="invalid-feedback" role="alert">
-									<strong>{{ $message }}</strong>
-								</span>
-							@enderror
-						</div>
-
-						<div class="form-group row form_input">
-							<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-							<label for="password-confirm" class="col-md-4 col-form-label text-md-right label_name">
-								<span class="content_name">{{ __('Confirm Password') }}</span>
-							</label>
-						</div>
-						<input type="submit" class="btn btn-primary" value="{{ __('Register') }}">
-						<div class="form_p">
-							<a href="{{route('login')}}">have an account <span class="signup login_form">signin</span></a>
-						</div>
-					</form>
 				</div>
-			</div>
-			@error('user_register')
-			<span class="invalid-feedback" role="alert">
-				<h3>{{ $message }}</h3>
-			</span>
-			@enderror
+
+				<div class="auth_input">
+					<label for="last_name" class="">
+						<span class="content_name">{{ __('Last Name') }}</span>
+					</label>
+					<input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+					@error('last_name')
+						<span class="invalid-feedback" role="alert">
+							<strong>{{ $message }}</strong>
+						</span>
+					@enderror
+				</div>
+
+				<div class="auth_input">
+					<label for="email" class="">
+						<span class="content_name">{{ __('E-Mail Address') }}</span>
+					</label>
+					<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+					@error('email')
+					<span class="invalid-feedback" role="alert">
+						<strong>{{ $message }}</strong>
+					</span>
+					@enderror
+				</div>
+
+				<div class="auth_input">
+					<label for="password" class="">
+						<span class="content_name">{{ __('Password') }}</span>
+					</label>
+					<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+					@error('password')
+						<span class="invalid-feedback" role="alert">
+							<strong>{{ $message }}</strong>
+						</span>
+					@enderror
+				</div>
+
+				<div class="auth_input">
+					<label for="password-confirm" class="">
+						<span class="content_name">{{ __('Confirm Password') }}</span>
+					</label>
+					<input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+				</div>
+				<input type="submit" class="btn btn-primary" value="{{ __('Register') }}">
+				<div class="">
+					<a class="auth_options" href="{{route('login')}}">have an account <span class="signup login_form">signin</span></a>
+				</div>
+			</form>
 		</div>
-	</div>
+	</aside>
 </section>
 @endsection
